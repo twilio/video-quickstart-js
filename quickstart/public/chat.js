ed6d43f849f9c4fed4bd6795534a330ff07f557b
Jackson <jsharf@salesforce.com>
2018-01-24 17:29:06 -0800

    generate names and show name

--- quickstart/public/chat.js
+++ quickstart/public/chat.js
@@ -30,13 +30,27 @@ function getTokenAndSetupChat(memberName,endpointId)
         });
 }
 
+function index(min, max) {
+  	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function generateName(){
+	var name1 = ["Adam", "Alex", "Aaron", "Ben", "Carl", "Dan", "David", "Edward", "Fred", "Frank", "George", "Hal", "Hank", "Ike", "John", "Jack", "Joe", "Larry", "Monte", "Matthew", "Mark", "Nathan", "Otto", "Paul", "Peter", "Roger", "Roger", "Steve", "Thomas", "Tim", "Ty", "Victor", "Walter"];
+
+	var name2 = ["Anderson", "Ashwoon", "Aikin", "Bateman", "Bongard", "Bowers", "Boyd", "Cannon", "Cast", "Deitz", "Dewalt", "Ebner", "Frick", "Hancock", "Haworth", "Hesch", "Hoffman", "Kassing", "Knutson", "Lawless", "Lawicki", "Mccord", "McCormack", "Miller", "Myers", "Nugent", "Ortiz", "Orwig", "Ory", "Paiser", "Pak", "Pettigrew", "Quinn", "Quizoz", "Ramachandran", "Resnick", "Sagar", "Schickowski", "Schiebel", "Sellon", "Severson", "Shaffer", "Solberg", "Soloman", "Sonderling", "Soukup", "Soulis", "Stahl", "Sweeney", "Tandy", "Trebil", "Trusela", "Trussel", "Turco", "Uddin", "Uflan", "Ulrich", "Upson", "Vader", "Vail", "Valente", "Van Zandt", "Vanderpoel", "Ventotla", "Vogal", "Wagle", "Wagner", "Wakefield", "Weinstein", "Weiss", "Woo", "Yang", "Yates", "Yocum", "Zeaser", "Zeller", "Ziegler", "Bauer", "Baxster", "Casal", "Cataldi", "Caswell", "Celedon", "Chambers", "Chapman", "Christensen", "Darnell", "Davidson", "Davis", "DeLorenzo", "Dinkins", "Doran", "Dugelman", "Dugan", "Duffman", "Eastman", "Ferro", "Ferry", "Fletcher", "Fietzer", "Hylan", "Hydinger", "Illingsworth", "Ingram", "Irwin", "Jagtap", "Jenson", "Johnson", "Johnsen", "Jones", "Jurgenson", "Kalleg", "Kaskel", "Keller", "Leisinger", "LePage", "Lewis", "Linde", "Lulloff", "Maki", "Martin", "McGinnis", "Mills", "Moody", "Moore", "Napier", "Nelson", "Norquist", "Nuttle", "Olson", "Ostrander", "Reamer", "Reardon", "Reyes", "Rice", "Ripka", "Roberts", "Rogers", "Root", "Sandstrom", "Sawyer", "Schlicht", "Schmitt", "Schwager", "Schutz", "Schuster", "Tapia", "Thompson", "Tiernan", "Tisler"];
+
+	var name = name1[index(0, name1.length + 1)] + ' ' + name2[index(0, name2.length + 1)];
+	return name;
+
+}
+
 function startChat(cobrowserId,sessionKey)
 {
     $('#chatHistory').empty();
     // hard code type to public, 2 for private
     channelType="1";
     channelName=$('#room-name').val();
-    memberName="";
+    memberName=generateName();
 
     if (channelName === "")
     {
@@ -196,7 +210,7 @@ function printMessage(auth,msg)
             "<img src='http://clipart-library.com/img1/754540.png' alt='' width='32' height='32'>"+
             "</div>"+
             "<div class='chat-message-content'>" +
-            "<h5>"+msg+"</h5>"+
+            "<h5><strong>"+auth + ": </strong>" + msg+"</h5>"+
             //"<span class='chat-time right'>13:35</span>"+
             //"<p class='right'>Me</p>"+
             "</div> <!-- end chat-message-content -->"+
