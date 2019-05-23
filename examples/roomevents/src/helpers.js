9fe44db93c83bec60864403c994658c589964773
Makarand Patwardhan <mpatwardhan@twilio.com>
2019-05-22 18:22:09 -0700

    WIP

--- examples/roomevents/src/helpers.js
+++ examples/roomevents/src/helpers.js
@@ -3,37 +3,19 @@
 var Video = require('twilio-video');
 
 /**
- * add/removes css attribute per dominant speaker change.
- * @param {Participant} speaker - Participant
- * @param {boolean} add - boolean true when new speaker is detected. false for old speaker
- * @returns {void}
- */
-function updateDominantSpeaker(speaker, add) {
-  if (speaker) {
-    const participantDiv = document.getElementById(speaker.sid);
-    if (participantDiv) {
-      participantDiv.classList[add ? 'add' : 'remove']('dominent_speaker');
-    }
-  }
-}
-
-/**
  * Creates a Room and handles dominant speaker changes.
  * @param {string} token - Token for joining the Room
  * @returns {CancelablePromise<Room>}
  */
-let dominantSpeaker = null;
-function createRoomAndUpdateOnSpeakerchange(token) {
+function createRoomAndUpdateOnStateChange(token, onStateChange) {
   return Video.connect(token, {
     dominantSpeaker: true,
   }).then(function(room) {
-    room.on('dominantSpeakerChanged', function(participant) {
-      updateDominantSpeaker(dominantSpeaker, false);
-      dominantSpeaker = participant;
-      updateDominantSpeaker(dominantSpeaker, true);
-    });
+    room.on('disconnected', onStateChange);
+    room.on('reconnected', onStateChange);
+    room.on('reconnecting', onStateChange);
     return room;
   });
 }
 
-exports.createRoomAndUpdateOnSpeakerchange = createRoomAndUpdateOnSpeakerchange;
+exports.createRoomAndUpdateOnStateChange = createRoomAndUpdateOnStateChange;
--- examples/roomevents/src/index.js
+++ examples/roomevents/src/index.js
@@ -5,70 +5,39 @@ const Video = require('twilio-video');
 const getRoomCredentials = require('../../util/getroomcredentials');
 const getSnippet = require('../../util/getsnippet');
 const helpers = require('./helpers');
-const createRoomAndUpdateOnSpeakerchange = helpers.createRoomAndUpdateOnSpeakerchange;
-const connectNewUserBtn = document.querySelector('input#connectNewUser');
+const createRoomAndUpdateOnStateChange = helpers.createRoomAndUpdateOnStateChange;
+const connectOrDisconnect = document.querySelector('input#connectordisconnect');
 const mediaContainer = document.getElementById('remote-media');
-const userControls = document.getElementById('user-controls');
 let roomName = null;
+let room = null;
+let someRoom = null;
 
-/**
- * creates a button and adds to given container.
- */
-function createButton(text, container) {
-  const btn = document.createElement('button');
-  btn.innerHTML = text; // 'Disconnect';
-  btn.classList.add('btn', 'btn-outline-primary', 'btn-sm');
-  container.appendChild(btn);
-  return btn;
-}
-/**
- *
- * creates controls for user to mute/unmute and disconnect
- * from the room.
- */
-function createUserControls(room) {
-  const localUser = room.localParticipant;
-  const currentUserControls = document.createElement('div');
-  currentUserControls.classList.add('usercontrol');
-
-  const title = document.createElement('h6');
-  title.appendChild(document.createTextNode(localUser.identity));
-  currentUserControls.appendChild(title);
-
-  const disconnectBtn = createButton('Disconnect', currentUserControls);
-  disconnectBtn.onclick = function() {
-    room.disconnect();
-    currentUserControls.parentNode.removeChild(currentUserControls);
-  }
-
-  // mute button.
-  const muteBtn = createButton('Mute', currentUserControls);
-  muteBtn.onclick = function() {
-    const mute = muteBtn.innerHTML == 'Mute';
-    getTracks(localUser).forEach(function(track) {
-      if (track.kind === 'audio') {
-        if (mute) {
-          track.disable();
-        } else {
-          track.enable();
-        }
-      }
-    });
-    muteBtn.innerHTML = mute ? 'Unmute' : 'Mute';
-  }
-  userControls.appendChild(currentUserControls);
-}
 
 /**
  * Connect the Participant with media to the Room.
  */
 async function connectToRoom() {
+  console.log("connectToRoom start");
   const creds = await getRoomCredentials();
-  const room = await Video.connect( creds.token, {
+  room = await Video.connect( creds.token, {
     name: roomName
   });
+  connectOrDisconnect.value = 'Disconnect from Room';
+}
 
-  createUserControls(room);
+/**
+ * Disconnect the Participant with media from the Room.
+ */
+function disconnectFromRoom() {
+  room.disconnect();
+  room = null;
+  connectOrDisconnect.value = 'Connect to Room';
+  return;
+}
+
+function connectToOrDisconnectFromRoom(event) {
+  event.preventDefault();
+  return room ? disconnectFromRoom() : connectToRoom();
 }
 
 /**
@@ -80,6 +49,21 @@ function getTracks(participant) {
   }).map(function(publication) {
     return publication.track;
   });
+}
+
+/**
+ * Get the Tracks of the given Participant.
+ */
+let lastRoomState = "unknown";
+function onRoomStateChange(newState) {
+  console.log("StateChange: ", newState);
+
+  const oldStateBtn = document.getElementById("roomstate-" + lastRoomState);
+  oldStateBtn.classList.remove('current');
+
+  lastRoomState = someRoom.state;
+  const newStateBtn = document.getElementById("roomstate-" + someRoom.state);
+  newStateBtn.classList.add('current');
 }
 
 (async function() {
@@ -95,35 +79,31 @@ function getTracks(participant) {
   // Connect to a random Room with no media. This Participant will
   // display the media of the other Participants that will enter
   // the Room and watch for dominant speaker updates.
-  const someRoom = await createRoomAndUpdateOnSpeakerchange(creds.token);
+  someRoom = await createRoomAndUpdateOnStateChange(creds.token, onRoomStateChange);
+  onRoomStateChange();
+
+  console.log("makarand: created room!", someRoom.name);
+
+  // Set the name of the Room to which the Participant that shares
+  // media should join.
+  roomName = someRoom.name;
 
   // set listener to connect new users to the room.
-  connectNewUserBtn.style.display = 'block';
-  connectNewUserBtn.onclick = function(event) {
-    event.preventDefault();
-    connectToRoom();
-  };
+  connectOrDisconnect.style.display = 'block';
+  connectOrDisconnect.onclick = connectToOrDisconnectFromRoom;
 
   // Disconnect from the Room on page unload.
   window.onbeforeunload = function() {
     someRoom.disconnect();
   };
 
-  // Set the name of the Room to which the Participant that shares
-  // media should join.
-  roomName = someRoom.name;
-
   someRoom.on('participantConnected', function(participant) {
-    const div = document.createElement('div');
-    div.id = participant.sid;
-
-    const title = document.createElement('h6');
-    title.appendChild(document.createTextNode(participant.identity));
-    div.appendChild(title);
-
-    mediaContainer.appendChild(div);
+    console.log("participantConnected: ", participant);
+    // const div = document.createElement('div');
+    // div.id = participant.sid;
+    // mediaContainer.appendChild(div);
     participant.on('trackSubscribed', function(track) {
-      div.appendChild(track.attach());
+      mediaContainer.appendChild(track.attach());
     });
   });
 
