0a62e24746ea1058f57ca05242d0e89bd3a78675
Makarand Patwardhan <makarandp@yahoo.com>
2019-06-10 12:18:32 -0700

    media selection sample that works on mobile devices and connect to room (#80)

--- examples/mediadevices/src/helpers.js
+++ examples/mediadevices/src/helpers.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 'use strict';
 
 var Video = require('twilio-video');
@@ -15,41 +16,81 @@ function getDevicesOfKind(deviceInfos, kind) {
 }
 
 /**
- * Apply the selected audio input device.
- * @param {string} deviceId
- * @param {HTMLAudioElement} audio
- * @returns {Promise<void>}
+ * Replace the existing LocalAudioTrack or LocalVideoTrack with
+ * a new one in the Room.
+ * @param {Room} room - The Room you have joined
+ * @param {LocalAudioTrack|LocalVideoTrack} track - The LocalTrack you want to switch to
+ * @returns {void}
  */
-function applyAudioInputDeviceSelection(deviceId, audio) {
-  return Video.createLocalAudioTrack({
-    deviceId: deviceId
-  }).then(function(localTrack) {
-    localTrack.attach(audio);
+function switchLocalTracks(room, track) {
+  room.localParticipant.tracks.forEach(function(trackPublication) {
+    if (trackPublication.kind === track.kind) {
+      trackPublication.track.stop();
+      room.localParticipant.unpublishTrack(trackPublication.track);
+    }
   });
+  room.localParticipant.publishTrack(track);
 }
 
 /**
  * Apply the selected audio output device.
  * @param {string} deviceId
  * @param {HTMLAudioElement} audio
+ * @returns {Promise<void>}
  */
 function applyAudioOutputDeviceSelection(deviceId, audio) {
-  audio.setSinkId(deviceId);
+  // return typeof audio.setSinkId === 'function'
+  //   ? audio.setSinkId(deviceId)
+  //   : Promise.reject('This browser does not support setting an audio output device');
+
+  if (typeof audio.setSinkId === 'function') {
+    return audio.setSinkId(deviceId);
+  }
+  return Promise.reject('This browser does not support setting an audio output device');
+
+}
+
+/**
+ * Apply the selected audio input device.
+ * @param {string} deviceId
+ * @param {HTMLAudioElement} audio
+ * @param {Room} [room] - The Room, if you have already joined one
+ * @returns {Promise<void>}
+ */
+function applyAudioInputDeviceSelection(deviceId, audio, room) {
+  return Video.createLocalAudioTrack({
+    deviceId: {
+      exact: deviceId // NOTE: on ios safari - it respects the deviceId only if its exact.
+    }
+  }).then(function(localTrack) {
+    localTrack.attach(audio);
+    if (room) {
+      switchLocalTracks(room, localTrack);
+    }
+  }).catch(function(error) {
+    console.log('applyAudioInputDeviceSelection failed:', error);
+  });
 }
 
 /**
  * Apply the selected video input device.
  * @param {string} deviceId
  * @param {HTMLVideoElement} video
+ * @param {Room} [room] - The Room, if you have already joined one
  * @returns {Promise<void>}
  */
-function applyVideoInputDeviceSelection(deviceId, video) {
+function applyVideoInputDeviceSelection(deviceId, video, room) {
   return Video.createLocalVideoTrack({
-    deviceId: deviceId,
-    height: 240,
-    width: 320
+    deviceId: {
+      exact: deviceId
+    }
   }).then(function(localTrack) {
     localTrack.attach(video);
+    if (room) {
+      switchLocalTracks(room, localTrack);
+    }
+  }).catch(function(error) {
+    console.log('applyVideoInputDeviceSelection failed:', error);
   });
 }
 
@@ -63,7 +104,7 @@ function applyVideoInputDeviceSelection(deviceId, video) {
  */
 function getDeviceSelectionOptions() {
   return navigator.mediaDevices.enumerateDevices().then(function(deviceInfos) {
-    var kinds = [ 'audioinput', 'audiooutput', 'videoinput' ];
+    var kinds = ['audioinput', 'audiooutput', 'videoinput'];
     return kinds.reduce(function(deviceSelectionOptions, kind) {
       deviceSelectionOptions[kind] = getDevicesOfKind(deviceInfos, kind);
       return deviceSelectionOptions;
@@ -71,7 +112,22 @@ function getDeviceSelectionOptions() {
   });
 }
 
+/**
+ * Connects to room using specified input devices
+ * @param {string} token
+ * @param {string} audioDeviceId
+ * @param {string} videoDeviceId
+ * @returns {Promise<Room>}
+ */
+function connectWithSelectedDevices(token, audioDeviceId, videoDeviceId) {
+  return Video.connect(token, {
+    audio: { deviceId: { exact: audioDeviceId } },
+    video: { deviceId: { exact: videoDeviceId } }
+  });
+}
+
 module.exports.applyAudioInputDeviceSelection = applyAudioInputDeviceSelection;
 module.exports.applyAudioOutputDeviceSelection = applyAudioOutputDeviceSelection;
 module.exports.applyVideoInputDeviceSelection = applyVideoInputDeviceSelection;
+module.exports.connectWithSelectedDevices = connectWithSelectedDevices;
 module.exports.getDeviceSelectionOptions = getDeviceSelectionOptions;
