1688f4ec170f266a3802c35bb4cc524e5782c78f
joma <joma@twilio.com>
2020-02-14 11:19:51 -0800

    Made the reconnection events helper function.

--- examples/remoteReconnection/src/helpers.js
+++ examples/remoteReconnection/src/helpers.js
@@ -2,7 +2,27 @@
 
 /**
  * Listen to Room reconnection events and update the UI accordingly.
- * @param {Participant} participant - The Room you have joined
+ * @param {Room} room - The Room you have joined
  * @param {function} updateRoomState - Updates the app UI with the new Room state
  * @returns {void}
  */
+
+ function remoteReconnectionUpdates (room, updateRemoteState) {
+  room.on('participantReconnecting', remoteParticipant => {
+    if (remoteParticipant.state === 'reconnecting') {
+      console.log('Remote Participant is Reconnecting')
+    }
+    // Make UI reconnecting bar orange here
+    updateRemoteState(remoteParticipant.state)
+  })
+
+  room.on('participantReconnected', remoteParticipant => {
+    if (remoteParticipant.state === 'connected') {
+      console.log('Remote Participant is reconnected')
+    }
+    // Make UI connected bar green here
+    updateRemoteState(remoteParticipant.state)
+  })
+ }
+
+exports.remoteReconnectionUpdates = remoteReconnectionUpdates;
--- examples/remoteReconnection/src/index.js
+++ examples/remoteReconnection/src/index.js
@@ -17,40 +17,49 @@ const remoteMedia = document.getElementById('remote-media');
   pre.innerHTML = Prism.highlight(snippet, Prism.languages.javascript);
 
   // Get the credentials to connect to the Room.
-  const credsLocal = await getRoomCredentials();
-  const credsRemote = await getRoomCredentials();
+  const credsP1 = await getRoomCredentials();
+  const credsP2 = await getRoomCredentials();
 
   // Create room instance and name for participants to join.
-  const roomLocal = await Video.connect(credsLocal.token);
+  const roomP1 = await Video.connect(credsP1.token);
   
   // Set room name for participant 2 to join.
-  const roomName = roomLocal.name;
+  const roomName = roomP1.name;
 
   // Connecting remote participants.
-  const roomRemote = await Video.connect(credsRemote.token, {
+  const roomP2 = await Video.connect(credsP2.token, {
     name: roomName
   });
 
   // Appends video/audio tracks when each participant is subscribed.
-  roomLocal.on('trackSubscribed', track => {
+  roomP1.on('trackSubscribed', track => {
     if (track.isEnabled) {
       remoteMedia.appendChild(track.attach())
     } 
   })
   
-  roomRemote.on('trackSubscribed', track => {
-    if (track.isEnabled) {
-      localMedia.appendChild(track.attach())
-    } 
-  })
+  // roomP2.on('trackSubscribed', track => {
+  //   if (track.isEnabled) {
+  //     localMedia.appendChild(track.attach())
+  //   } 
+  // })
 
   // Reconnecting state simulator button
-
+    // Interrupts network when button is clicked
+  
+  // Update UI of remote and local participant when remote participants are in a reconnecting state
+    roomP1.on('participantConnected', remoteParticipant => {
+      // UPDATE UI OF P2/ REMOTE PARTICIPANT TO CONNECTED : GREEN COLOR
+    })
+    roomP1.on('participantReconnecting', remoteParticipant => {
+      // UPDATE UI OF P2/ REMOTE PARTICIPANT TO RECONNECTING : ORANGE COLOR
+    })
+  
 
   // Disconnect from the Room 
   window.onbeforeunload = () => {
-    roomLocal.disconnect();
-    roomRemote.disconnect();
+    roomP1.disconnect();
+    roomP2.disconnect();
     roomName = null;
   }
 }())
