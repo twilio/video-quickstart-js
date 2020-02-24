8658cc45dbdd1d695c30254de949f5dd4c3f82fb
Joyce Ma <43423318+PikaJoyce@users.noreply.github.com>
2020-02-24 11:20:38 -0800

    Incorporating PR feedback

--- examples/remotereconnection/src/helpers.js
+++ examples/remotereconnection/src/helpers.js
@@ -1,30 +1,35 @@
 'use strict';
 
 /**
- * Listen to Room reconnection events and update the UI accordingly.
+ * Listen to RemoteParticipant reconnection events and update the UI accordingly.
  * @param {Room} room - The Room you have joined
  * @param {function} updateRoomState - Updates the app UI with the new state
  * @returns {void}
  */
-
- function handleRemoteParticipantReconnectionUpdates(room, updateParticipantState) {
-  room.on('participantReconnecting', participant => {
+function handleRemoteParticipantReconnectionUpdates(room, updateParticipantState) {
+  room.on('participantReconnecting', function(participant) {
     updateParticipantState(participant.state);
   });
 
-  room.on('participantReconnected', participant => {
+  room.on('participantReconnected', function(participant) {
     updateParticipantState(participant.state);
   });
 }
 
+/**
+ * Listen to LocalParticipant reconnection events and update the UI accordingly.
+ * @param {Room} room - The Room you have joined
+ * @param {function} updateRoomState - Updates the app UI with the new state
+ * @returns {void}
+ */
 function handleLocalParticipantReconnectionUpdates(room, updateParticipantState) {
   const localParticipant = room.localParticipant;
 
-  localParticipant.on('reconnecting', () => {
+  localParticipant.on('reconnecting', function() {
     updateParticipantState(localParticipant.state);
   });
 
-  localParticipant.on('reconnected', () => {
+  localParticipant.on('reconnected', function() {
     updateParticipantState(localParticipant.state);
   });
 } 
