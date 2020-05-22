352a2e4a6f0281a5cc459a1e31726cb94b8842ca
joma <joma@twilio.com>
2020-05-22 16:32:56 -0700

    More scaffolding

--- examples/datatracks/src/index.js
+++ examples/datatracks/src/index.js
@@ -2,3 +2,34 @@
 
 const Prism = require('prismjs');
 const Video = require('twilio-video');
+
+// Create Room Name
+
+// Create localDataTracks
+  /**
+   * Setup a LocalAudioTrack and LocalVideoTrack to render to a <video> element.
+   * @param {HTMLVideoElement} video
+   * @returns {Promise<Array<LocalAudioTrack|LocalVideoTrack>>} audioAndVideoTrack
+   */
+  // async function setupLocalAudioAndVideoTracks(video) {
+  //   const audioAndVideoTrack = await createLocalTracks();
+  //   audioAndVideoTrack.forEach(track => track.attach(video));
+  //   return audioAndVideoTrack;
+  // }
+
+  //   const dataTrack = setupLocalDataTrack();
+  //   const videoTrack = await setupLocalAudioAndVideoTracks(video);
+
+// Concat tracks with Audio & Video track (not doing audio)
+  //  const tracks = audioAndVideoTrack.concat(dataTrack);
+
+
+// Connect P1 with {room: roomName, tracks:ALLTRACKSHERE} on button click
+
+// Attach video to P1 DOM
+
+// OPEN A NEW WINDOW
+
+// Connect P2 with {room: roomName, tracks: ALLTRACKSHERE} on button click
+
+//
