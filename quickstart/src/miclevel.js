d591843db77319cbf17e6b8d055f80aa6697626b
Joyce Ma <43423318+PikaJoyce@users.noreply.github.com>
2021-02-23 11:20:05 -0800

    VIDEO-3563 Small patch to fix audio level indicator (#171)
    * VIDEO-3563 Small patch to audio level indicator
    
    Co-authored-by: joma <joma@twilio.com>

--- quickstart/src/miclevel.js
+++ quickstart/src/miclevel.js
@@ -15,39 +15,57 @@ function rootMeanSquare(samples) {
 
 /**
  * Poll the microphone's input level.
- * @param stream - the MediaStream representing the microphone
+ * @param audioTrack - the AudioTrack representing the microphone
  * @param maxLevel - the calculated level should be in the range [0 - maxLevel]
  * @param onLevel - called when the input level changes
  */
-module.exports = audioContext ? function micLevel(stream, maxLevel, onLevel) {
-  audioContext.resume().then(() => {
-    const analyser = audioContext.createAnalyser();
-    analyser.fftSize = 1024;
-    analyser.smoothingTimeConstant = 0.5;
+module.exports = audioContext
+  ? function micLevel(audioTrack, maxLevel, onLevel) {
+      audioContext.resume().then(() => {
+        let rafID;
 
-    const source = audioContext.createMediaStreamSource(stream);
-    source.connect(analyser);
-    const samples = new Uint8Array(analyser.frequencyBinCount);
+        const initializeAnalyser = () => {
+          const analyser = audioContext.createAnalyser();
+          analyser.fftSize = 1024;
+          analyser.smoothingTimeConstant = 0.5;
 
-    const track = stream.getTracks()[0];
-    let level = null;
+          const stream = new MediaStream([audioTrack.mediaStreamTrack]);
+          const audioSource = audioContext.createMediaStreamSource(stream);
+          const samples = new Uint8Array(analyser.frequencyBinCount);
 
-    requestAnimationFrame(function checkLevel() {
-      analyser.getByteFrequencyData(samples);
-      const rms = rootMeanSquare(samples);
-      const log2Rms = rms && Math.log2(rms);
-      const newLevel = Math.ceil(maxLevel * log2Rms / 8);
+          audioSource.connect(analyser);
+          startAnimation(analyser, samples);
+        };
 
-      if (level !== newLevel) {
-        level = newLevel;
-        onLevel(level);
-      }
+        initializeAnalyser();
 
-      requestAnimationFrame(track.readyState === 'ended'
-        ? () => onLevel(0)
-        : checkLevel);
-    });
-  });
-} : function notSupported() {
-  // Do nothing.
-};
+        // We listen to when the Audio Track is started, and once it is,
+        // the Analyser Node is restarted.
+        audioTrack.on('started', initializeAnalyser);
+
+        let level = null;
+
+        function startAnimation(analyser, samples) {
+          window.cancelAnimationFrame(rafID);
+
+          rafID = requestAnimationFrame(function checkLevel() {
+            analyser.getByteFrequencyData(samples);
+            const rms = rootMeanSquare(samples);
+            const log2Rms = rms && Math.log2(rms);
+            const newLevel = Math.ceil((maxLevel * log2Rms) / 8);
+
+            if (level !== newLevel) {
+              level = newLevel;
+              onLevel(level);
+            }
+
+            rafID =  requestAnimationFrame(audioTrack.mediaStreamTrack.readyState === 'ended'
+              ? () => onLevel(0)
+              : checkLevel);
+          });
+        }
+      });
+    }
+  : function notSupported() {
+      // Do nothing.
+    };
